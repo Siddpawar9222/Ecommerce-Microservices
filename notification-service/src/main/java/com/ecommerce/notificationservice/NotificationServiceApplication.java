f9ddd9d1e8cfa93a694b2fce7eb3403cef493bbc
Siddhesh <siddpawar583@gmail.com>
2024-02-03 12:08:06 +0530

    Kafka Bug Fix

--- notification-service/src/main/java/com/ecommerce/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/ecommerce/notificationservice/NotificationServiceApplication.java
@@ -3,10 +3,12 @@ package com.ecommerce.notificationservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
